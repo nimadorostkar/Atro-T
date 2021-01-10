32de4164f85e67634cbdfc40fd938fa5a78e1118
nimadorostkar <nimadorostkar97@gmail.com>
2021-01-10 15:17:43 +0330

    fork and copy from atro dashboard

---
+++ app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, re_path
+from app import views
+
+urlpatterns = [
+    # Matches any html file
+    re_path(r'^.*\.html', views.pages, name='pages'),
+
+    # The home page
+    path('', views.index, name='home'),
+]
